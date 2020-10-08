8fe26b39741ad55f2b54a5d78583bf4b3b04ff46
manh <manh@nimblehq.co>
2020-10-08 09:56:56 +0700

    Add common-rx module

---
+++ common-rx/src/main/java/co/nimblehq/common/transformers/Transformers.kt
@@ -0,0 +1,14 @@
+package co.nimblehq.common.transformers
+
+import io.reactivex.Observable
+
+object Transformers {
+
+    /**
+     * Emits the latest value of the `source` Observable whenever the `when`
+     * Observable emits.
+     */
+    fun <S, T> takeWhen(`when`: Observable<T>): TakeWhenTransformer<S, T> {
+        return TakeWhenTransformer(`when`)
+    }
+}
