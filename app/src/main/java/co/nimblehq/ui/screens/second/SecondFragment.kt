74e392898e99f5b8e757a1148b8b61c518828be1
Luong Vo <luong@nimblehq.co>
2021-01-13 15:44:13 +0700

    Add RxPermissions to handle CAMERA permission on Second screen

--- app/src/main/java/co/nimblehq/ui/screens/second/SecondFragment.kt
+++ app/src/main/java/co/nimblehq/ui/screens/second/SecondFragment.kt
@@ -1,20 +1,25 @@
 package co.nimblehq.ui.screens.second
 
+import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import co.nimblehq.R
 import co.nimblehq.extension.subscribeOnClick
 import co.nimblehq.ui.base.BaseFragment
 import co.nimblehq.ui.screens.home.Data
+import com.tbruyelle.rxpermissions2.Permission
+import com.tbruyelle.rxpermissions2.RxPermissions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_second.*
+import javax.inject.Inject
 
-/**
- * TODO update with a new permission requesting
- */
-//@RuntimePermissions
 @AndroidEntryPoint
 class SecondFragment : BaseFragment() {
+
+    @Inject
+    lateinit var rxPermission: RxPermissions
 
     override val layoutRes: Int = R.layout.fragment_second
 
@@ -24,7 +29,7 @@ class SecondFragment : BaseFragment() {
 
     override fun setupView() {
         btOpenCamera
-            .subscribeOnClick { toaster.display("Not yet implemented") }
+            .subscribeOnClick(::requestCamera)
             .addToDisposables()
     }
 
@@ -34,29 +39,28 @@ class SecondFragment : BaseFragment() {
         viewModel.output.persistData bindTo ::bindPersistedData
     }
 
-//    @NeedsPermission(Manifest.permission.CAMERA)
-//    fun openCamera() {
-//        startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
-//    }
-//
-//    @OnPermissionDenied(Manifest.permission.CAMERA)
-//    fun showDeniedForCamera() {
-//        Toast.makeText(this, "Permission camera denied", Toast.LENGTH_SHORT).show()
-//    }
-//
-//    @OnNeverAskAgain(Manifest.permission.CAMERA)
-//    fun showNeverAskForCamera() {
-//        Toast.makeText(this, "Permission camera never ask", Toast.LENGTH_SHORT).show()
-//    }
+    private fun requestCamera() {
+        rxPermission
+            .requestEach(Manifest.permission.CAMERA)
+            .subscribe(::handleCameraPermission)
+            .addToDisposables()
+    }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        onRequestPermissionsResult(requestCode, permissions, grantResults)
-//    }
+    private fun handleCameraPermission(permission: Permission) {
+        when {
+            permission.granted -> {
+                startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+            }
+            permission.shouldShowRequestPermissionRationale -> {
+                // Deny
+                toaster.display("Permission camera denied")
+            }
+            else -> {
+                // Deny and never ask again
+                toaster.display("Permission camera never ask")
+            }
+        }
+    }
 
     private fun bindPersistedData(data: Data) {
         // TODO: Refactor view's naming
