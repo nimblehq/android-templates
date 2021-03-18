e93f8ad82d26ab77b63f4b5d5f96ff5a20a3f28f
tuubz <tobias@nimblehq.co>
2021-03-18 14:51:16 +0700

    Create dependency injection modules

---
+++ CoroutineTemplate/app/src/main/java/co/nimblehq/coroutine/di/modules/AppModule.kt
@@ -0,0 +1,9 @@
+package co.nimblehq.coroutine.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
