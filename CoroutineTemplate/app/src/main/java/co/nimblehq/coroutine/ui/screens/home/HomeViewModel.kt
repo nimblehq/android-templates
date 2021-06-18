f5dda0bcffe228fb2d435147c428229323188406
Chorn <kritchai@nimblehq.co>
2021-06-18 18:00:44 +0700

    Integrate Flow to CoroutineTemplate

--- CoroutineTemplate/app/src/main/java/co/nimblehq/coroutine/ui/screens/home/HomeViewModel.kt
+++ CoroutineTemplate/app/src/main/java/co/nimblehq/coroutine/ui/screens/home/HomeViewModel.kt
@@ -1,22 +1,23 @@
 package co.nimblehq.coroutine.ui.screens.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import co.nimblehq.coroutine.domain.data.entity.UserEntity
-import co.nimblehq.coroutine.domain.usecase.UseCaseResult
 import co.nimblehq.coroutine.domain.usecase.GetUsersUseCase
-import co.nimblehq.coroutine.lib.Event
+import co.nimblehq.coroutine.domain.usecase.UseCaseResult
 import co.nimblehq.coroutine.ui.base.BaseViewModel
+import co.nimblehq.coroutine.ui.base.NavigationEvent
+import co.nimblehq.coroutine.ui.screens.second.SecondBundle
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 interface Output {
 
-    val users: LiveData<List<UserEntity>>
+    val users: StateFlow<List<UserEntity>>
 
-    val showError: LiveData<Event<String>>
+    fun navigateToSecond(bundle: SecondBundle)
 }
 
 @HiltViewModel
@@ -24,25 +25,28 @@ class HomeViewModel @Inject constructor(
     private val getUsersUseCase: GetUsersUseCase
 ) : BaseViewModel(), Output {
 
-    // TODO: Re-factor LiveData to Flow and remove Event
-    private val _users = MutableLiveData<List<UserEntity>>()
-    override val users: LiveData<List<UserEntity>>
+    private val _users = MutableStateFlow<List<UserEntity>>(listOf())
+    override val users: StateFlow<List<UserEntity>>
         get() = _users
-
-    private val _showError = MutableLiveData<Event<String>>()
-    override val showError: LiveData<Event<String>>
-        get() = _showError
 
     init {
         fetchUsers()
     }
 
+    override fun navigateToSecond(bundle: SecondBundle) {
+        viewModelScope.launch {
+            _navigator.emit(NavigationEvent.Second(bundle))
+        }
+    }
+
     private fun fetchUsers() {
+        showLoading()
         viewModelScope.launch {
             when (val result = getUsersUseCase.execute()) {
                 is UseCaseResult.Success -> _users.value = result.data
-                is UseCaseResult.Error -> _showError.value = Event(result.exception.message.orEmpty())
+                is UseCaseResult.Error -> _error.emit(result.exception.message.orEmpty())
             }
+            hideLoading()
         }
     }
 }
