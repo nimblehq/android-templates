b95379910858e7cf3c83f5abd735993a207c8285
Minh Pham <minh@nimblehq.co>
2021-07-02 13:44:37 +0700

    [98] Bring BaseSharedPreferences to CoroutineTemplate

---
+++ CoroutineTemplate/domain/src/main/java/co/nimblehq/coroutine/domain/storage/BaseSharedPreferences.kt
@@ -0,0 +1,40 @@
+package co.nimblehq.coroutine.domain.storage
+
+import android.content.SharedPreferences
+import co.nimblehq.coroutine.domain.extension.execute
+
+abstract class BaseSharedPreferences {
+
+    protected lateinit var sharedPreferences: SharedPreferences
+
+    protected fun <T> set(key: String, value: T?) {
+        sharedPreferences.execute {
+            when (value) {
+                null -> it.remove(key)
+                is Boolean -> it.putBoolean(key, value)
+                is String -> it.putString(key, value)
+                is Float -> it.putFloat(key, value)
+                is Long -> it.putLong(key, value)
+                is Int -> it.putInt(key, value)
+            }
+        }
+    }
+
+    protected inline fun <reified T> get(key: String): T? =
+        if (sharedPreferences.contains(key)) {
+            when (T::class) {
+                Boolean::class -> sharedPreferences.getBoolean(key, false) as T?
+                String::class -> sharedPreferences.getString(key, null) as T?
+                Float::class -> sharedPreferences.getFloat(key, 0f) as T?
+                Int::class -> sharedPreferences.getInt(key, 0) as T?
+                Long::class -> sharedPreferences.getLong(key, 0L) as T?
+                else -> null
+            }
+        } else {
+            null
+        }
+
+    protected fun clearAll() {
+        sharedPreferences.execute { it.clear() }
+    }
+}
