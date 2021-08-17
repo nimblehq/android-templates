4fc0222d028ca2f441d9883ea6b0433756a1bac0
Luong Vo <luong@nimblehq.co>
2021-08-17 17:31:31 +0700

    [#115] Add and setup Skeleton loading mechanism

--- RxJavaTemplate/app/src/main/java/co/nimblehq/rxjava/ui/base/BaseFragment.kt
+++ RxJavaTemplate/app/src/main/java/co/nimblehq/rxjava/ui/base/BaseFragment.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import android.view.*
 import android.view.View.*
 import androidx.annotation.CallSuper
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import co.nimblehq.rxjava.R
 import co.nimblehq.rxjava.domain.schedulers.SchedulerProvider
 import co.nimblehq.rxjava.extension.hideSoftKeyboard
 import co.nimblehq.rxjava.extension.subscribeOnClick
 import co.nimblehq.rxjava.ui.common.Toaster
 import co.nimblehq.rxjava.ui.userReadableMessage
+import com.faltenreich.skeletonlayout.*
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
+
+private const val SKELETON_ITEM_COUNT_DEFAULT = 2
 
 @SuppressWarnings("TooManyFunctions")
 abstract class BaseFragment<VB : ViewBinding> : Fragment(), BaseFragmentCallbacks {
@@ -54,6 +60,8 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment(), BaseFragmentCallback
     }
 
     private val disposables = CompositeDisposable()
+    private lateinit var skeletonConfig: SkeletonConfig
+    private val skeletons: HashMap<Int, Skeleton> = HashMap()
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -85,6 +93,13 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment(), BaseFragmentCallback
     @CallSuper
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        skeletonConfig = SkeletonConfig.default(requireContext()).apply {
+            maskCornerRadius =
+                resources.getDimensionPixelSize(R.dimen.skeleton_mask_corner_radius_circle)
+                    .toFloat()
+        }
+
         (this as? BaseFragmentCallbacks)?.let {
             setWindowStyle()
             setupView()
@@ -116,6 +131,30 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment(), BaseFragmentCallback
         observeOn(schedulerProvider.main())
             .subscribe { action(it) }
             .addToDisposables()
+    }
+
+    protected fun SkeletonLayout.applySkeleton(customMaskCornerRadius: Float? = null) {
+        skeletons[id] = apply {
+            maskCornerRadius = customMaskCornerRadius ?: skeletonConfig.maskCornerRadius
+            maskColor = skeletonConfig.maskColor
+            shimmerColor = skeletonConfig.shimmerColor
+        }
+    }
+
+    protected fun RecyclerView.applySkeleton(@LayoutRes listItemLayoutResId: Int) {
+        skeletons[id] = applySkeleton(
+            listItemLayoutResId = listItemLayoutResId,
+            itemCount = SKELETON_ITEM_COUNT_DEFAULT,
+            config = skeletonConfig
+        )
+    }
+
+    protected fun showOrHideSkeletonLoading(view: View, isLoading: Boolean) {
+        if (isLoading) {
+            skeletons[view.id]?.showSkeleton()
+        } else {
+            skeletons[view.id]?.showOriginal()
+        }
     }
 
     private fun setWindowStyle() {
