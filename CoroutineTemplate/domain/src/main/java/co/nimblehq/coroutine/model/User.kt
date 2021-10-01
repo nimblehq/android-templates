cd1bc1eb2f5258eb7576c71904ef3c4a87df1cc9
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-10-01 15:27:55 +0700

    [#131] Update functions name of `UserResponse`to make them more clear

--- CoroutineTemplate/domain/src/main/java/co/nimblehq/coroutine/model/User.kt
+++ CoroutineTemplate/domain/src/main/java/co/nimblehq/coroutine/model/User.kt
@@ -4,7 +4,7 @@ data class User(
     val id: Int?,
     val name: String,
     val username: String,
-    val email: String?,
+    val email: String,
     val address: Address?,
     val phone: String,
     val website: String
--- CoroutineTemplate/domain/src/main/java/co/nimblehq/coroutine/response/UserResponse.kt
+++ CoroutineTemplate/domain/src/main/java/co/nimblehq/coroutine/response/UserResponse.kt
@@ -8,51 +8,51 @@ data class UserResponse(
     @Json(name = "name") val name: String?,
     @Json(name = "username") val username: String?,
     @Json(name = "email") val email: String?,
-    @Json(name = "address") val address: Address?,
+    @Json(name = "address") val addressResponse: AddressResponse?,
     @Json(name = "phone") val phone: String?,
     @Json(name = "website") val website: String?
 ) {
-    data class Address(
+    data class AddressResponse(
         @Json(name = "street") val street: String?,
         @Json(name = "suite") val suite: String?,
         @Json(name = "city") val city: String?,
         @Json(name = "zipcode") val zipCode: String?,
-        @Json(name = "geo") val geo: Geo?
+        @Json(name = "geo") val geoResponse: GeoResponse?
     ) {
-        data class Geo(
+        data class GeoResponse(
             @Json(name = "lat") val latitude: String?,
             @Json(name = "lng") val longitude: String?
         )
     }
 }
 
-fun List<UserResponse>.toUserModel(): List<User> {
-    return this.map { it.toModel() }
+fun List<UserResponse>.toUser(): List<User> {
+    return this.map { it.toUser() }
 }
 
-private fun UserResponse.toModel(): User {
+private fun UserResponse.toUser(): User {
     return User(
         id = this.id,
         name = this.name.orEmpty(),
         username = this.username.orEmpty(),
         email = this.email.orEmpty(),
-        address = this.address?.toModel(),
+        address = this.addressResponse?.toAddress(),
         phone = this.phone.orEmpty(),
         website = this.website.orEmpty()
     )
 }
 
-private fun UserResponse.Address.toModel(): User.Address {
+private fun UserResponse.AddressResponse.toAddress(): User.Address {
     return User.Address(
         street = this.street.orEmpty(),
         suite = this.suite.orEmpty(),
         city = this.city.orEmpty(),
         zipCode = this.zipCode.orEmpty(),
-        geo = this.geo?.toModel()
+        geo = this.geoResponse?.toGeo()
     )
 }
 
-private fun UserResponse.Address.Geo.toModel(): User.Address.Geo {
+private fun UserResponse.AddressResponse.GeoResponse.toGeo(): User.Address.Geo {
     return User.Address.Geo(
         latitude = this.latitude.orEmpty(),
         longitude = this.longitude.orEmpty()
