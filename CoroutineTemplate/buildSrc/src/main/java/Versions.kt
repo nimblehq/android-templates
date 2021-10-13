cbfef2104a895000ba75a1811def31357e354717
hoangnguyen92dn <hoang.l@nimblehq.co>
2021-10-13 14:29:51 +0700

    [#111] Add buildSrc folder

---
+++ CoroutineTemplate/buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,38 @@
+object Versions {
+    const val BUILD_GRADLE_VERSION = "4.2.1"
+
+    const val ANDROID_COMPILE_SDK_VERSION = 30
+    const val ANDROID_MIN_SDK_VERSION = 23
+    const val ANDROID_TARGET_SDK_VERSION = 30
+
+    const val ANDROID_VERSION_CODE = 1
+    const val ANDROID_VERSION_NAME = "3.2.0"
+
+    // Dependencies (Alphabet sorted)
+    const val ANDROID_CRYPTO_VERSION = "1.0.0"
+    const val ANDROIDX_ACTIVITY_KTX_VERSION = "1.2.1"
+    const val ANDROIDX_CORE_KTX_VERSION = "1.3.0"
+    const val ANDROIDX_LIFECYCLE_VERSION = "2.4.0-alpha02"
+    const val ANDROIDX_NAVIGATION_VERSION = "2.3.4"
+    const val ANDROIDX_SUPPORT_VERSION = "1.3.0"
+
+    const val COMPOSE_VERSION = "1.0.2"
+    const val CONSTRAINT_LAYOUT_VERSION = "2.0.0-alpha3"
+
+    const val JAVAX_INJECT_VERSION = "1"
+
+    const val HILT_VERSION = "2.38.1"
+
+    const val KOTLIN_VERSION = "1.5.21"
+    const val KOTLINX_COROUTINES_VERSION = "1.5.0"
+
+    const val MOSHI_VERSION = "1.12.0"
+
+    const val OKHTTP_VERSION = "4.9.1"
+    const val RETROFIT_VERSION = "2.9.0"
+
+    const val TIMBER_LOG_VERSION = "4.7.1"
+
+    // Configuration
+    const val DETEKT_VERSION = "1.18.1"
+}
