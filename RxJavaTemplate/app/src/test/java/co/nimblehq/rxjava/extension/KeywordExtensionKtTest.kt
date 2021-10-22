47f155d585987584d74094fabd6d127324557789
manh <manh@nimblehq.co>
2021-10-22 14:05:09 +0700

    [#83] Skip the test instead of comment it

--- RxJavaTemplate/app/src/test/java/co/nimblehq/rxjava/extension/KeywordExtensionKtTest.kt
+++ RxJavaTemplate/app/src/test/java/co/nimblehq/rxjava/extension/KeywordExtensionKtTest.kt
@@ -1,6 +1,7 @@
 package co.nimblehq.rxjava.extension
 
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 
 class KeywordExtensionKtTest {
@@ -11,15 +12,16 @@ class KeywordExtensionKtTest {
      * Reference issue: https://issuetracker.google.com/issues/197065758
      * Solution: Update to AGP >7.1 will fix the issue
      */
+    @Ignore
     @Test
     fun `using unless doesn't execute if the condition is satisfied`() {
-//        var result = 3
-//        val condition: Int = -1
-//
-//        unless(condition > -1) { result = 4 }
-//        assertEquals("block should exec", 4, result)
-//
-//        unless(condition == -1) { result = 5 }
-//        assertEquals("block should NOT exec", 4, result)
+        var result = 3
+        val condition: Int = -1
+
+        unless(condition > -1) { result = 4 }
+        assertEquals("block should exec", 4, result)
+
+        unless(condition == -1) { result = 5 }
+        assertEquals("block should NOT exec", 4, result)
     }
 }
