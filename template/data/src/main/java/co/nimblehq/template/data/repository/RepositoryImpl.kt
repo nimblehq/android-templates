54873511291f4c26c51cc8aa166b552edbb82900
Luong Vo <luong@nimblehq.co>
2022-05-05 16:35:48 +0700

    [#213] Apply to use Flow instead of `suspend fun`, get rid of `UseCaseResult`

--- template/data/src/main/java/co/nimblehq/template/data/repository/RepositoryImpl.kt
+++ template/data/src/main/java/co/nimblehq/template/data/repository/RepositoryImpl.kt
@@ -4,10 +4,19 @@ import co.nimblehq.template.data.response.toModels
 import co.nimblehq.template.data.service.ApiService
 import co.nimblehq.template.domain.model.Model
 import co.nimblehq.template.domain.repository.Repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class RepositoryImpl constructor(
     private val apiService: ApiService
 ) : Repository {
 
-    override suspend fun getModels(): List<Model> = apiService.getResponses().toModels()
+    override fun getModels(): Flow<List<Model>> = flow {
+        try {
+            val result = apiService.getResponses().toModels()
+            emit(result)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }
