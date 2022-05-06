76dbcbe02df04a1ccb8a79e67e1c73f21ae972a3
Tobias (Toby) Heuts <tobias@nimblehq.co>
2022-05-06 17:07:12 +0700

    [#198] Support arguments and store in variable

--- newproject.kts
+++ newproject.kts
@@ -1,8 +1,23 @@
 object NewProject {
 
-    fun generate() {
+    private const val KEY_APP_NAME = "app-name"
+    private const val KEY_PACKAGE_NAME = "package-name"
+    private const val ARGUMENT_DELIMITER = "="
 
+    private var appName = ""
+    private var packageName = ""
+
+    fun generate(args: Array<String>) {
+        handleArguments(args)
+    }
+
+    private fun handleArguments(args: Array<String>) {
+        args.forEach {
+            val (key, value) = it.split(ARGUMENT_DELIMITER)
+            if (key == KEY_APP_NAME) appName = value
+            if (key == KEY_PACKAGE_NAME) packageName = value
+        }
     }
 }
 
-NewProject.generate()
+NewProject.generate(args)
