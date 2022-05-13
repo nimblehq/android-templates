791cbb0e6fc919951099d4d68c4825d409ea977a
Tran Manh <manh@nimblehq.co>
2022-05-13 17:47:18 +0700

    [#202][#203][#204][#205] Rename folder structure for existing modules

--- scripts/new_project.kts
+++ scripts/new_project.kts
@@ -3,10 +3,13 @@ import java.io.File
 object NewProject {
 
     private const val ARGUMENT_DELIMITER = "="
+    private const val DOT_SEPARATOR = "."
     private const val KEY_APP_NAME = "app-name"
     private const val KEY_PACKAGE_NAME = "package-name"
     private const val TEMPLATE_FOLDER_NAME = "CoroutineTemplate"
     private const val TEMPLATE_PACKAGE_NAME = "co.nimblehq.coroutine"
+
+    private val modules = listOf("app", "data", "domain")
 
     private var appName = ""
     private val appNameWithoutSpace: String
@@ -21,6 +24,7 @@ object NewProject {
         handleArguments(args)
         initializeNewProjectFolder()
         cleanNewProjectFolder()
+        repackage()
         renamePackageNameWithinFiles()
     }
 
@@ -41,6 +45,55 @@ object NewProject {
         }
     }
 
+    private fun repackage() {
+        showMessage("=> 🔎 Rename the package folders...")
+        modules.forEach { module ->
+            val srcPath = projectPath + File.separator + module + File.separator + "src"
+            for (file in File(srcPath).listFiles()) {
+                if (file.isDirectory) {
+                    val javaPath = file.absolutePath + File.separator + "java"
+                    val currentPackagePath =
+                        javaPath + File.separator + TEMPLATE_PACKAGE_NAME.replace(
+                            DOT_SEPARATOR,
+                            File.separator
+                        )
+                    val newPackagePath =
+                        javaPath + File.separator + packageName.replace(
+                            DOT_SEPARATOR,
+                            File.separator
+                        )
+                    if (File(currentPackagePath).exists()) {
+                        copyCurrentPackageToNewPackageAndClean(
+                            currentPackagePath = currentPackagePath,
+                            tempPath = javaPath + File.separator + "temp_folder",
+                            newPackagePath = newPackagePath
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun copyCurrentPackageToNewPackageAndClean(
+        currentPackagePath: String,
+        tempPath: String,
+        newPackagePath: String
+    ) {
+        copyFiles(currentPackagePath, tempPath)
+        deleteFilesOrDirectories(
+            currentPackagePath.replace(
+                TEMPLATE_PACKAGE_NAME.replace(DOT_SEPARATOR, File.separator),
+                TEMPLATE_PACKAGE_NAME.split(DOT_SEPARATOR).first()
+            )
+        )
+        copyFiles(tempPath, newPackagePath)
+        deleteFilesOrDirectories(tempPath)
+    }
+
+    private fun deleteFilesOrDirectories(fromPath: String) {
+        File(fromPath).deleteRecursively()
+    }
+
     private fun showMessage(message: String) {
         println("\n${message}\n")
     }
@@ -57,8 +110,8 @@ object NewProject {
     private fun cleanNewProjectFolder() {
         executeCommand("sh $projectPath${File.separator}gradlew -p $projectPath clean")
         executeCommand("sh $projectPath${File.separator}gradlew -p $projectPath${File.separator}buildSrc clean")
-        listOf(".idea",".gradle","buildSrc${File.separator}.gradle",".git").forEach {
-            File( "$projectPath${File.separator}$it")?.let { targetFile ->
+        listOf(".idea", ".gradle", "buildSrc${File.separator}.gradle", ".git").forEach {
+            File("$projectPath${File.separator}$it")?.let { targetFile ->
                 targetFile.deleteRecursively()
             }
         }
