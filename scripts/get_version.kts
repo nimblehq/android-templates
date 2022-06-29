2f15922928981186503f084dfabfb9d786940103
Avishek Khan <avishek@nimblehq.co>
2022-06-29 12:15:22 +0700

    [#100] Update regex pattern to account for any number of digits to search for version in the commit message

--- scripts/get_version.kts
+++ scripts/get_version.kts
@@ -2,7 +2,7 @@ import java.io.File
 
 object GetVersion {
 
-    private const val PATTERN_VERSION = "[0-9].[0-9].[0-9]"
+    private const val PATTERN_VERSION = "[0-9]+.[0-9]+.[0-9]+"
 
     fun search(commitMessage: String): String? {
         return PATTERN_VERSION
