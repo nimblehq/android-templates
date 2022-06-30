d9821deaa5da2458cebf882b57e1aca26050cd9c
Avishek Khan <avishek@nimblehq.co>
2022-06-30 10:46:18 +0700

    [#100] Fix pattern

--- scripts/get_version.kts
+++ scripts/get_version.kts
@@ -1,6 +1,6 @@
 object GetVersion {
 
-    private const val PATTERN_VERSION = "[0-9]+.[0-9]+.[0-9]+"
+    private const val PATTERN_VERSION = "[0-9]+[.][0-9]+[.][0-9]+"
 
     fun search(commitMessage: String): String? {
         return PATTERN_VERSION
