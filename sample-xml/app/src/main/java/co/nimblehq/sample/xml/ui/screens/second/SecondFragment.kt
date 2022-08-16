614a404c9f08cdb553ddc10edb2d0aea26396eda
lydiasama <hallelujah.lydia@gmail.com>
2022-08-16 17:17:24 +0700

    [#243] Remove initial code of SecondFragment and SecondViewModel

--- sample-xml/app/src/main/java/co/nimblehq/sample/xml/ui/screens/second/SecondFragment.kt
+++ sample-xml/app/src/main/java/co/nimblehq/sample/xml/ui/screens/second/SecondFragment.kt
@@ -1,36 +1,10 @@
 package co.nimblehq.sample.xml.ui.screens.second
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import co.nimblehq.sample.xml.databinding.FragmentHomeBinding
-import co.nimblehq.sample.xml.extension.provideViewModels
-import co.nimblehq.sample.xml.model.UiModel
-import co.nimblehq.sample.xml.ui.base.BaseFragment
-import co.nimblehq.sample.xml.ui.screens.MainNavigator
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class SecondFragment : BaseFragment<FragmentHomeBinding>() {
+class SecondFragment : Fragment() {
 
-    @Inject
-    lateinit var navigator: MainNavigator
-
-    private val viewModel: SecondViewModel by provideViewModels()
-
-    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
-        get() = { inflater, container, attachToParent ->
-            FragmentHomeBinding.inflate(inflater, container, attachToParent)
-        }
-
-    override fun bindViewModel() {
-        viewModel.uiModels bindTo ::displayUiModels
-        viewModel.error bindTo toaster::display
-        viewModel.navigator bindTo navigator::navigate
-    }
-
-    private fun displayUiModels(uiModels: List<UiModel>) {
-        Timber.d("Result : $uiModels")
-    }
+    // TODO: Implement SecondFragment in Part2
 }
