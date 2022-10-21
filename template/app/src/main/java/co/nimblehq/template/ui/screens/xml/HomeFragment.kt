f561148898b824867439e1b7e1e5c493d3f55548
Ryan Conway <ryan@nimblehq.co>
2022-10-21 11:21:48 +0700

    [#261] Remove sample code from template

--- template/app/src/main/java/co/nimblehq/template/ui/screens/xml/HomeFragment.kt
+++ template/app/src/main/java/co/nimblehq/template/ui/screens/xml/HomeFragment.kt
@@ -2,15 +2,12 @@ package co.nimblehq.template.ui.screens.xml
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
 import co.nimblehq.template.databinding.FragmentHomeBinding
 import co.nimblehq.template.extension.provideViewModels
 import co.nimblehq.template.model.UiModel
 import co.nimblehq.template.ui.base.BaseFragment
 import co.nimblehq.template.ui.screens.MainNavigator
-import com.markodevcic.peko.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -31,17 +28,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         viewModel.uiModels bindTo ::displayUiModels
         viewModel.error bindTo ::displayError
         viewModel.navigator bindTo navigator::navigate
-        viewModel.requestPermissions bindTo ::requestPermissions
     }
 
     private fun displayUiModels(uiModels: List<UiModel>) {
         Timber.d("Result : $uiModels")
-    }
-
-    private fun requestPermissions(vararg permissions: String) {
-        lifecycleScope.launch {
-            val result = requestPermissionsAsync(*permissions)
-            viewModel.onPermissionResult(result)
-        }
     }
 }
--- template/app/src/main/java/co/nimblehq/template/ui/screens/xml/HomeViewModel.kt
+++ template/app/src/main/java/co/nimblehq/template/ui/screens/xml/HomeViewModel.kt
@@ -1,16 +1,12 @@
 package co.nimblehq.template.ui.screens.xml
 
-import android.Manifest
 import co.nimblehq.template.domain.usecase.UseCase
 import co.nimblehq.template.model.UiModel
 import co.nimblehq.template.model.toUiModels
 import co.nimblehq.template.ui.base.BaseViewModel
 import co.nimblehq.template.util.DispatchersProvider
-import com.markodevcic.peko.*
-import com.markodevcic.peko.PermissionResult.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,10 +18,6 @@ class HomeViewModel @Inject constructor(
     private val _uiModels = MutableStateFlow<List<UiModel>>(emptyList())
     val uiModels: StateFlow<List<UiModel>>
         get() = _uiModels
-
-    private val _requestPermissions = MutableStateFlow<Array<out String>>(emptyArray())
-    val requestPermissions: StateFlow<Array<out String>>
-        get() = _requestPermissions
 
     init {
         execute {
@@ -39,27 +31,6 @@ class HomeViewModel @Inject constructor(
                     _uiModels.emit(uiModels)
                 }
             hideLoading()
-
-            requestPermissions()
-        }
-    }
-
-    private suspend fun requestPermissions() {
-        _requestPermissions.emit(
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.CAMERA
-            )
-        )
-    }
-
-    fun onPermissionResult(permissionResult: PermissionResult) {
-        when (permissionResult) {
-            is Granted -> Timber.d("${permissionResult.grantedPermissions} granted")
-            is Denied.JustDenied -> Timber.d("${permissionResult.deniedPermissions} denied")
-            is Denied.NeedsRationale -> Timber.d("${permissionResult.deniedPermissions} needs rationale")
-            is Denied.DeniedPermanently -> Timber.d("${permissionResult.deniedPermissions} denied for good")
-            is Cancelled -> Timber.d("request cancelled")
         }
     }
 }
