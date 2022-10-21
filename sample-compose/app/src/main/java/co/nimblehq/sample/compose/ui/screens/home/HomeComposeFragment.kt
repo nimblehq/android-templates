09322f9ca31413e481e6b27e67d0dbda72f9fd27
Ryan Conway <ryan@nimblehq.co>
2022-10-21 11:30:24 +0700

    [#261] Add sample permission request code to sample-compose

--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/screens/home/HomeComposeFragment.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/screens/home/HomeComposeFragment.kt
@@ -2,10 +2,13 @@ package co.nimblehq.sample.compose.ui.screens.home
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.lifecycleScope
 import co.nimblehq.sample.compose.extension.provideViewModels
 import co.nimblehq.sample.compose.ui.base.BaseComposeFragment
 import co.nimblehq.sample.compose.ui.screens.MainNavigator
+import com.markodevcic.peko.requestPermissionsAsync
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,5 +31,13 @@ class HomeComposeFragment : BaseComposeFragment() {
     override fun bindViewModel() {
         viewModel.error bindTo ::displayError
         viewModel.navigator bindTo navigator::navigate
+        viewModel.requestPermissions bindTo ::requestPermissions
+    }
+
+    private fun requestPermissions(vararg permissions: String) {
+        lifecycleScope.launch {
+            val result = requestPermissionsAsync(*permissions)
+            viewModel.onPermissionResult(result)
+        }
     }
 }
--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/screens/home/HomeComposeViewModel.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/screens/home/HomeComposeViewModel.kt
@@ -1,13 +1,17 @@
 package co.nimblehq.sample.compose.ui.screens.home
 
+import android.Manifest
 import co.nimblehq.sample.compose.domain.usecase.UseCase
 import co.nimblehq.sample.compose.model.UiModel
 import co.nimblehq.sample.compose.model.toUiModels
 import co.nimblehq.sample.compose.ui.base.BaseViewModel
 import co.nimblehq.sample.compose.ui.base.NavigationEvent
 import co.nimblehq.sample.compose.util.DispatchersProvider
+import com.markodevcic.peko.PermissionResult
+import com.markodevcic.peko.PermissionResult.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import timber.log.Timber
 import javax.inject.Inject
 
 // TODO: Rename to 'HomeViewModel'
@@ -21,6 +25,10 @@ class HomeComposeViewModel @Inject constructor(
     val uiModels: StateFlow<List<UiModel>>
         get() = _uiModels
 
+    private val _requestPermissions = MutableStateFlow<Array<out String>>(emptyArray())
+    val requestPermissions: StateFlow<Array<out String>>
+        get() = _requestPermissions
+
     init {
         execute {
             showLoading()
@@ -33,10 +41,31 @@ class HomeComposeViewModel @Inject constructor(
                     _uiModels.emit(uiModels)
                 }
             hideLoading()
+
+            requestPermissions()
         }
     }
 
     fun navigateToSecond(uiModel: UiModel) {
         execute { _navigator.emit(NavigationEvent.Second(uiModel)) }
+    }
+
+    private suspend fun requestPermissions() {
+        _requestPermissions.emit(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CAMERA
+            )
+        )
+    }
+
+    fun onPermissionResult(permissionResult: PermissionResult) {
+        when (permissionResult) {
+            is Granted -> Timber.d("${permissionResult.grantedPermissions} granted")
+            is Denied.JustDenied -> Timber.d("${permissionResult.deniedPermissions} denied")
+            is Denied.NeedsRationale -> Timber.d("${permissionResult.deniedPermissions} needs rationale")
+            is Denied.DeniedPermanently -> Timber.d("${permissionResult.deniedPermissions} denied for good")
+            is Cancelled -> Timber.d("request cancelled")
+        }
     }
 }
