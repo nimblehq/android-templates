cc91780fae09730dc5da3df5d13451b0328c746a
Luong Vo <luong@nimblehq.co>
2023-01-06 14:15:33 +0700

    [#353] Remove unnecessary sample for themeColors, add sample for custom TopAppBar background color

--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/AppColors.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/AppColors.kt
@@ -2,28 +2,24 @@ package co.nimblehq.sample.compose.ui.theme
 
 import androidx.compose.material.*
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 
 /**
  * Extend final [Colors] class to provide more custom app colors.
  */
 data class AppColors(
-    val themeColors: Colors
+    val themeColors: Colors,
 
     // Custom colors here
+    val topAppBarBackground: Color = GreenCitrus
 )
 
 internal val LightColorPalette = AppColors(
-    themeColors = lightColors(
-        primary = GreenCitrus,
-        secondary = GreenChristi
-    )
+    themeColors = lightColors()
 )
 
 internal val DarkColorPalette = AppColors(
-    themeColors = darkColors(
-        primary = GreenCitrus,
-        secondary = GreenChristi
-    )
+    themeColors = darkColors()
 )
 
 internal val LocalColors = staticCompositionLocalOf { LightColorPalette }
--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/Color.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/Color.kt
@@ -2,5 +2,5 @@ package co.nimblehq.sample.compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Base colors here
 internal val GreenCitrus = Color(0xFF99CC00)
-internal val GreenChristi = Color(0xFF669900)
