7126d1030645fd30512af90edfe9ed0649971004
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-01-31 17:07:23 +0700

    [#377] Remove url from UiModel

--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/model/UiModel.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/model/UiModel.kt
@@ -6,8 +6,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UiModel(
-    val id: String = "",
-    val url: String = "https://www.google.com/"
+    val id: String = ""
 ) : Parcelable
 
 private fun Model.toUiModel() = UiModel(id.toString())
