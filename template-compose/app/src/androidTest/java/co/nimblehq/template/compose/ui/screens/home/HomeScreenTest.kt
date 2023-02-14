1eaf35321417537c18aa4fc3c9969880db87a3f2
Luong Vo <luong@nimblehq.co>
2023-02-14 15:47:44 +0700

    [#354] Clean up unused imports

--- template-compose/app/src/androidTest/java/co/nimblehq/template/compose/ui/screens/home/HomeScreenTest.kt
+++ template-compose/app/src/androidTest/java/co/nimblehq/template/compose/ui/screens/home/HomeScreenTest.kt
@@ -1,14 +1,13 @@
 package co.nimblehq.template.compose.ui.screens.home
 
 import androidx.activity.compose.setContent
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.compose.ui.test.onNodeWithText
 import co.nimblehq.template.compose.R
 import co.nimblehq.template.compose.ui.AppDestination
 import co.nimblehq.template.compose.ui.screens.MainActivity
 import org.junit.*
-import org.junit.runner.RunWith
 
 class HomeScreenTest {
 
