068afe2c3dbb379f7947df52637aa9f24505c1ac
Luong Vo <luong@nimblehq.co>
2023-04-27 13:58:11 +0700

    [#427] Make the showToast ext to be more flexible

--- template-compose/app/src/main/java/co/nimblehq/template/compose/extensions/ContextExt.kt
+++ template-compose/app/src/main/java/co/nimblehq/template/compose/extensions/ContextExt.kt
@@ -3,5 +3,5 @@ package co.nimblehq.template.compose.extensions
 import android.content.Context
 import android.widget.Toast
 
-fun Context.showToast(message: String) =
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) =
+    Toast.makeText(this, message, duration).show()
