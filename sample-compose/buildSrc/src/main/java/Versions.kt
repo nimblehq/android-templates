8972a1eff8349db04e7daae0240329143829977b
ryan-conway <ryan-conway@users.noreply.github.com>
2023-08-03 02:48:52 +0000

    [Chore] Bump version to 3.23.0

--- sample-compose/buildSrc/src/main/java/Versions.kt
+++ sample-compose/buildSrc/src/main/java/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val ANDROID_TARGET_SDK_VERSION = 33
 
     const val ANDROID_VERSION_CODE = 1
-    const val ANDROID_VERSION_NAME = "3.22.0"
+    const val ANDROID_VERSION_NAME = "3.23.0"
 
     // Dependencies (Alphabet sorted)
     const val ACCOMPANIST_PERMISSIONS_VERSION = "0.30.1"
