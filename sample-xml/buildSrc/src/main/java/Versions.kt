3f232e37ea437a52ac85fc3d2d1428442da96a5a
ryan-conway <ryan-conway@users.noreply.github.com>
2023-08-31 09:21:33 +0000

    [Chore] Bump version to 3.24.0

--- sample-xml/buildSrc/src/main/java/Versions.kt
+++ sample-xml/buildSrc/src/main/java/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val ANDROID_TARGET_SDK_VERSION = 33
 
     const val ANDROID_VERSION_CODE = 1
-    const val ANDROID_VERSION_NAME = "3.23.0"
+    const val ANDROID_VERSION_NAME = "3.24.0"
 
     // Dependencies (Alphabet sorted)
     const val ANDROID_COMMON_KTX_VERSION = "0.1.1"
