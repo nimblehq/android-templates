06dc8f569881dd774b3fbaae6cf4adce94822abb
Huey <luonghoangdn92@gmail.com>
2023-10-19 09:42:46 +0700

    [#543] Add default spacing dimensions

--- template-compose/app/src/main/java/co/nimblehq/template/compose/ui/theme/AppDimensions.kt
+++ template-compose/app/src/main/java/co/nimblehq/template/compose/ui/theme/AppDimensions.kt
@@ -5,7 +5,14 @@ import androidx.compose.ui.unit.dp
 
 class AppDimensions {
     // Custom dimensions here
-    val spacingNormal = 16.dp
+    val spacing2XSmall = 4.dp
+    val spacingXSmall = 8.dp
+    val spacingSmall = 12.dp
+    val spacingMedium = 16.dp
+    val spacingLarge = 20.dp
+    val spacingXLarge = 24.dp
+    val spacing2XLarge = 28.dp
+    val spacing3XLarge = 32.dp
 }
 
 internal val LocalAppDimensions = staticCompositionLocalOf { AppDimensions() }
