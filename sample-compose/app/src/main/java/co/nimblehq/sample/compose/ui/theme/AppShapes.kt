ce96d8750b1c2a2cab199dd552168e39423d45a4
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2025-02-27 14:25:40 +0700

    [#603] Refactor to use Material3

--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/AppShapes.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/theme/AppShapes.kt
@@ -1,10 +1,15 @@
 package co.nimblehq.sample.compose.ui.theme
 
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.staticCompositionLocalOf
 
-private val Shapes = Shapes(
-    // Custom shapes here
-)
+interface AppShapes {
+    val themeShapes: Shapes
+        get() = Shapes(
+            // Custom shapes here
+        )
+}
 
-internal val LocalAppShapes = staticCompositionLocalOf { Shapes }
+object AppShapesImpl : AppShapes
+
+internal val LocalAppShapes = staticCompositionLocalOf<AppShapes> { AppShapesImpl }
