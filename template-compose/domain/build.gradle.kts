0ff08c4ee680ccdea374a75054c2da46111bae31
ThawZinToe <thawzin@nimblehq.co>
2026-01-29 09:56:53 +0700

    [#493] Refactor Dangerfile and CI configuration to improve report handling and update dependencies

--- template-compose/domain/build.gradle.kts
+++ template-compose/domain/build.gradle.kts
@@ -1,19 +1,12 @@
 plugins {
     id(libs.plugins.javaLibrary.get().pluginId)
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.android.lint)
     alias(libs.plugins.kover)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-}
-
-lint {
-    checkDependencies = true
-    xmlReport = true
-    xmlOutput = file("build/reports/lint/lint-result.xml")
 }
 
 dependencies {
