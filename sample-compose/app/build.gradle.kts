2ce377db6bdf83e28c1d9de194f00c2192db2027
Tiger <tantai@nimblehq.co>
2026-02-11 17:04:02 +0700

    [#617] Cleanup remaining nav2 logic and some unused codes

--- sample-compose/app/build.gradle.kts
+++ sample-compose/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
--- sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/models/UiModel.kt
+++ sample-compose/app/src/main/java/co/nimblehq/sample/compose/ui/models/UiModel.kt
@@ -1,16 +1,13 @@
 package co.nimblehq.sample.compose.ui.models
 
-import android.os.Parcelable
 import co.nimblehq.sample.compose.domain.models.Model
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Parcelize
 data class UiModel(
     val id: String,
     val username: String,
-) : Parcelable
+)
 
 fun Model.toUiModel() = UiModel(
     id.toString(),
