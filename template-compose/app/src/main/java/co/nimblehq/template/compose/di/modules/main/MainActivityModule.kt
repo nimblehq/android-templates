d43951a95c66fdb3ecf5a562d623f98dce7e2182
Tiger <tantai@nimblehq.co>
2026-02-26 17:55:32 +0700

    [#617] Migrate template compose to navigation 3

--- template-compose/app/src/main/java/co/nimblehq/template/compose/di/modules/main/MainActivityModule.kt
+++ template-compose/app/src/main/java/co/nimblehq/template/compose/di/modules/main/MainActivityModule.kt
@@ -1,9 +1,34 @@
 package co.nimblehq.template.compose.di.modules.main
 
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import co.nimblehq.template.compose.navigation.EntryProviderInstaller
+import co.nimblehq.template.compose.navigation.Navigator
+import co.nimblehq.template.compose.navigation.NavigatorImpl
+import co.nimblehq.template.compose.ui.screens.main.home.Home
+import co.nimblehq.template.compose.ui.screens.main.home.HomeScreen
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import dagger.multibindings.IntoSet
 
 @Module
-@InstallIn(ActivityComponent::class)
-class MainActivityModule
+@InstallIn(ActivityRetainedComponent::class)
+object MainActivityModule {
+
+    @Provides
+    @ActivityRetainedScoped
+    fun provideNavigator(): Navigator = NavigatorImpl(startDestination = Home)
+
+    @IntoSet
+    @Provides
+    fun provideEntryProviderInstaller(navigator: Navigator): EntryProviderInstaller = {
+        entry<Home> {
+            HomeScreen(
+                viewModel = hiltViewModel(),
+                navigator = navigator
+            )
+        }
+    }
+}
